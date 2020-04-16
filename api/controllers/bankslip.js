9c47feea3b41ee77dec040f23906457145c7480d
Rafael Magossi <rafael_mq2@hotmail.com>
2020-04-15 21:32:34 -0300

    Versao Final

--- api/controllers/bankslip.js
+++ api/controllers/bankslip.js
@@ -1,24 +1,63 @@
 require('dotenv').config()
-const pdfcrowd = require("pdfcrowd");
-const converApi = new pdfcrowd.HtmlToPdfClient("demo","ce544b6ea52a5621fb9d55f8b542d14d");
+
+const fs = require('fs');
+const pdf = require('html-pdf');
+const requestify = require('requestify');
 
 module.exports = app => {
     const controller = {};
     const urlBankslip = process.env.URL_BANKSLIP
+    const defaultPath = process.env.DEFAULT_PATH
+    const converterConfig = { directory: '/tmp', width: '210mm', height: '297mm'};
 
     controller.download = (req, res) => {
+        const _res = res
+        const { salesCode } = req.params;
 
-    const { salesCode } = req.params;
+        var externalURL = generateExternalUrl(salesCode);
+        var fullPath = generatePath(salesCode);
 
-    var url = `${urlBankslip}/${salesCode}`;
-    var callbacks = pdfcrowd.sendImageInHttpResponse(res, "application/pdf", `${salesCode}.pdf`, "attachment");
-    callbacks.error = function(errMessage, statusCode) {
-        res.set('Content-Type', 'text/plain');
-        res.status(statusCode || 400);
-        res.send(errMessage);
-    }
-    converApi.convertUrl(url, callbacks);
+        requestify.get(externalURL).then(function (response) {
+            var html = formatHtmlToConvertToPDF(response.body); 
+            pdf.create(html, converterConfig).toFile(fullPath, function (err, res) {
+                if (err) {
+                    return console.log(err);
+                }
+                console.log(`Download: ${res.filename}`); 
+                // _res.download(res.filename);
+                _res.download(res.filename,`${salesCode}.pdf`, function(err){
+                    deleteFile(res.filename);
+                });
+            });
+        });
     };
-  
-  return controller;
+
+    function formatHtmlToConvertToPDF(html){
+        return html.replace('body{background-color:#fff;margin-right:0}','body{background-color:#fff;zoom:.72;margin-right:0}')
+    }
+
+    function generateRandonNumber(){
+        return Math.floor( Math.random() * (999999 - 1 + 1) + 1)
+    }
+
+    function generatePath(salesCode){
+        var fileName = `${salesCode}${generateRandonNumber()}`
+        return `${defaultPath}/${fileName}.pdf`
+    }
+
+    function generateExternalUrl(salesCode){
+        return `${urlBankslip}/${salesCode}`;
+    }
+
+    function deleteFile (file) { 
+        fs.unlink(file, function (err) {
+            if (err) {
+                console.error(err.toString());
+            } else {
+                console.warn(`Deleted: ${file}`);
+            }
+        });
+    }
+
+    return controller;
 }
--- api/routes/bankslip.js
+++ api/routes/bankslip.js
@@ -2,5 +2,5 @@ module.exports = app => {
     const controller = app.controllers.bankslip;
 
     app.route('/api/v1/bankslip/download/:salesCode')
-    .get(controller.download)
+        .get(controller.download)
   }
